e4532f8f63533fe1b1badfd4bf946ba1ab314d41
oleg_dixon <olegdixon5973@gmail.com>
2025-03-29 22:23:22 +0500

    fix example.py

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -2,3 +2,23 @@ def reverse(string):
     return string[::-1]
 
 print(reverse('Hello!'))
+
+
+stack = []  # В Python стек реализован через список
+not stack  # Список пуст
+# True
+stack.append(1)  # [1]
+stack.append(2)  # [1, 2]
+stack.append(3)  # [1, 2, 3]
+not stack  # Список не пустой
+# False
+stack
+# [1, 2, 3]
+stack.pop()  # В стеке [1, 2]
+# 3
+stack.pop()  # В стеке [1]
+# 2
+stack.pop()  # В стеке пусто
+# 1
+print(not stack)
+# True
